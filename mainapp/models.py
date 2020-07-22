9f56ab88a53a2556e6350a80337010d87e4a4a56
Lucas Tostes <lucasrmtostes@gmail.com>
2020-07-21 23:03:04 -0300

    updated db fields

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,23 +4,32 @@ from django.db import models
 
 class Player(models.Model):
     name        = models.CharField(max_length=100)
-    description = models.CharField(max_length=5000,blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     # TO DO: Image field LOGO 
     # TO DO: Team COLORS
-    description = models.CharField(max_length=5000,blank=True)
-    anthem      = models.CharField(max_length=1000,blank=True, null=True)
+    description = models.TextField(blank=True)
+    anthem      = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
     
 
 class Edition(models.Model):
     name        = models.CharField(max_length=100)
-    description = models.CharField(max_length=5000,blank=True)
+    description = models.TextField(blank=True)
     start_date  = models.DateField()
     end_date    = models.DateField()
     location    = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Sport(models.Model):
@@ -32,7 +41,10 @@ class Sport(models.Model):
     # TO DO - Think of breakdown of types and how they are structured: Points or just win, single match for 4 teams or play-offs
     scoring_method = models.CharField(max_length=1, choices=SCORINGMETHOD_CHOICES,default='W')
     playoffs = models.BooleanField(default=False)
-    description = models.CharField(max_length=5000,blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.sport_category
 
 
 class Participant(models.Model):
@@ -40,11 +52,14 @@ class Participant(models.Model):
     team        = models.ForeignKey(Team, on_delete=models.SET_NULL, blank=True, null=True)
     edition     = models.ForeignKey(Edition, on_delete=models.CASCADE)
     
-    profile           = models.CharField(max_length=5000,blank=True, null=True)
+    profile           = models.TextField(blank=True, null=True)
     subscription_at   = models.DateTimeField(auto_now_add=True)
     update_at         = models.DateTimeField(auto_now=True)
     confirmation_at   = models.DateTimeField(blank=True, null=True)
     confirmed         = models.BooleanField(default = False)
+
+    def __str__(self):
+        return str(self.player) + ", " + str(self.team)
 
 
 class Competition(models.Model):
@@ -61,23 +76,32 @@ class Competition(models.Model):
     start_time    = models.DateTimeField(blank=True, null=True)
     finish_time   = models.DateTimeField(blank=True, null=True)
 
-    comments = models.CharField(max_length=5000,blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
 
     first_second_draw = models.BooleanField(default = False)
     second_third_draw = models.BooleanField(default = False)
     third_fourth_draw = models.BooleanField(default = False)
 
+    def __str__(self):
+        return str(self.sport) + ", " + str(self.edition)
+
 
 class Match(models.Model):
+    PLAYOFFROUND_CHOICES = [
+    ('SF1', 'Semi-final 1'),
+    ('SF2', 'Semi-final 2'),
+    ('F', 'Final'),
+    ('3', '3rd Place Dispute')
+]
     competition   = models.ForeignKey(Competition, on_delete=models.CASCADE)
-    description   = models.CharField(max_length=5000,blank=True, null=True)
+    description   = models.TextField(blank=True, null=True)
 
     started       = models.BooleanField(default = False)
     finished      = models.BooleanField(default = False)
     start_time    = models.DateTimeField(blank=True, null=True)
     finish_time   = models.DateTimeField(blank=True, null=True)
 
-    playoff_round = models.IntegerField(default = 1)
+    playoff_round = models.CharField(max_length=3, choices=PLAYOFFROUND_CHOICES,blank=True, null=True)
     
 
 class Score(models.Model):
@@ -100,5 +124,10 @@ class Note(models.Model):
     taken_by    = models.ForeignKey(Participant, on_delete=models.SET_NULL, blank=True, null=True, related_name='takenby_%(class)s')
     about       = models.ForeignKey(Participant, on_delete=models.CASCADE, blank=True, null=True, related_name='about_%(class)s')
 
+    comments = models.TextField(blank=True, null=True)
+
     created_at  = models.DateTimeField(auto_now_add=True)
     update_at   = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.taken_by) + ", sobre " + str(self.about)
